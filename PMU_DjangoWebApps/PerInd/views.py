bc982209730a7f4b5ce2e0920c5de302922365c8
yizongk <yizongk@gmail.com>
2020-07-14 01:34:58 -0400

    feat: Add Windows SSO support to PerInd Web App and add some links to the Allowed Host setting

--- PMU_DjangoWebApps/PerInd/views.py
+++ PMU_DjangoWebApps/PerInd/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def get_cur_client(request):
+    cur_client = request.META['REMOTE_USER']
+    return cur_client
+
+def index(request):
+    cur_client = get_cur_client(request)
+    return HttpResponse("Hello {}! You are at the PerInd Index".format(cur_client))
