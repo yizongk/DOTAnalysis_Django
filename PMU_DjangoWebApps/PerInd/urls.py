fb4a0ac2724ac3a27a752da89201cd65937cf0bc
ShainaLowenthal <slowenthal@dot.nyc.gov>
2020-07-15 17:35:00 -0400

    added templates and images folder

--- PMU_DjangoWebApps/PerInd/urls.py
+++ PMU_DjangoWebApps/PerInd/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 from .views import HomePageView, AboutPageView, ContactPageView
 urlpatterns = [
-    '''
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
-    '''
+    
+    #path('', views.index, name='index'),
+    #path('about', views.about, name='about'),
+    #path('contact', views.contact, name='contact'),
+    
     path('webgrid', views.webgrid, name='webgrid'),
      path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
