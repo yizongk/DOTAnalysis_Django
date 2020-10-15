521fbcdad3d6a3556ec70429b3d0e92d9b174c5d
yizongk <yizongk@gmail.com>
2020-10-15 12:13:11 -0400

    Remove useless permissions columns from data model

--- PMU_DjangoWebApps/PerInd/models.py
+++ PMU_DjangoWebApps/PerInd/models.py
@@ -8,10 +8,11 @@
 from django.db import models
 
 
+# All field names made lowercase.
 class Category(models.Model):
-    category_id = models.AutoField(db_column='Category_ID', primary_key=True)  # Field name made lowercase.
-    old_category_name = models.CharField(db_column='Old_Category_Name', max_length=255)  # Field name made lowercase.
-    category_name = models.CharField(db_column='Category_Name', max_length=255)  # Field name made lowercase.
+    category_id = models.AutoField(db_column='Category_ID', primary_key=True)
+    old_category_name = models.CharField(db_column='Old_Category_Name', max_length=255)
+    category_name = models.CharField(db_column='Category_Name', max_length=255)
 
     class Meta:
         managed = False
@@ -22,8 +23,8 @@ class Category(models.Model):
 
 
 class DataSummaryType(models.Model):
-    data_summary_id = models.AutoField(db_column='Data_Summary_ID', primary_key=True)  # Field name made lowercase.
-    summary_type = models.CharField(db_column='Summary_Type', max_length=255)  # Field name made lowercase.
+    data_summary_id = models.AutoField(db_column='Data_Summary_ID', primary_key=True)
+    summary_type = models.CharField(db_column='Summary_Type', max_length=255)
 
     class Meta:
         managed = False
@@ -34,8 +35,8 @@ class DataSummaryType(models.Model):
 
 
 class DataType(models.Model):
-    data_type_id = models.AutoField(db_column='Data_Type_ID', primary_key=True)  # Field name made lowercase.
-    data_type = models.CharField(db_column='Data_Type', max_length=255)  # Field name made lowercase.
+    data_type_id = models.AutoField(db_column='Data_Type_ID', primary_key=True)
+    data_type = models.CharField(db_column='Data_Type', max_length=255)
 
     class Meta:
         managed = False
@@ -46,8 +47,8 @@ class DataType(models.Model):
 
 
 class Unit(models.Model):
-    unit_id = models.AutoField(db_column='Unit_ID', primary_key=True)  # Field name made lowercase.
-    unit_type = models.CharField(db_column='Unit_Type', max_length=255)  # Field name made lowercase.
+    unit_id = models.AutoField(db_column='Unit_ID', primary_key=True)
+    unit_type = models.CharField(db_column='Unit_Type', max_length=255)
 
     class Meta:
         managed = False
@@ -62,11 +63,11 @@ ALTER TABLE [Users]
 ADD CONSTRAINT [AK_Users_Login] UNIQUE (Login);
 """
 class Users(models.Model):
-    user_id = models.AutoField(db_column='User_ID', primary_key=True)  # Field name made lowercase.
-    first_name = models.CharField(db_column='First_Name', max_length=255)  # Field name made lowercase.
-    last_name = models.CharField(db_column='Last_Name', max_length=255)  # Field name made lowercase.
-    login = models.CharField(db_column='Login', max_length=255, unique=True)  # Field name made lowercase.
-    active_user = models.BooleanField(db_column='Active_User', blank=True, null=True)  # Field name made lowercase.
+    user_id = models.AutoField(db_column='User_ID', primary_key=True)
+    first_name = models.CharField(db_column='First_Name', max_length=255)
+    last_name = models.CharField(db_column='Last_Name', max_length=255)
+    login = models.CharField(db_column='Login', max_length=255, unique=True)
+    active_user = models.BooleanField(db_column='Active_User', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -77,8 +78,8 @@ class Users(models.Model):
 
 
 class ValMultiplier(models.Model):
-    val_multiplier_id = models.AutoField(db_column='Val_Multiplier_ID', primary_key=True)  # Field name made lowercase.
-    multiplier_scale = models.IntegerField(db_column='Multiplier_Scale')  # Field name made lowercase.
+    val_multiplier_id = models.AutoField(db_column='Val_Multiplier_ID', primary_key=True)
+    multiplier_scale = models.IntegerField(db_column='Multiplier_Scale')
 
     class Meta:
         managed = False
@@ -102,10 +103,10 @@ SET [Fiscal_Year] = (
 GO
 """
 class YearMonth(models.Model):
-    year_month_id = models.AutoField(db_column='Year_Month_ID', primary_key=True)  # Field name made lowercase.
-    yyyy = models.IntegerField(db_column='YYYY')  # Field name made lowercase.
-    mm = models.IntegerField(db_column='MM')  # Field name made lowercase.
-    fiscal_year = models.IntegerField(db_column='Fiscal_Year')  # Field name made lowercase.
+    year_month_id = models.AutoField(db_column='Year_Month_ID', primary_key=True)
+    yyyy = models.IntegerField(db_column='YYYY')
+    mm = models.IntegerField(db_column='MM')
+    fiscal_year = models.IntegerField(db_column='Fiscal_Year')
 
     class Meta:
         managed = False
@@ -113,13 +114,12 @@ class YearMonth(models.Model):
 
 
 class UserPermissions(models.Model):
-    user_permission_id = models.AutoField(db_column='User_Permission_ID', primary_key=True)  # Field name made lowercase.
-    # user_id = models.IntegerField(db_column='User_ID', blank=True, null=True)  # Field name made lowercase.
-    # category_id = models.IntegerField(db_column='Category_ID', blank=True, null=True)  # Field name made lowercase.
-    read_right = models.BooleanField(db_column='Read_Right', blank=True, null=True)  # Field name made lowercase.
-    write_right = models.BooleanField(db_column='Write_Right', blank=True, null=True)  # Field name made lowercase.
-    user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    category = models.ForeignKey(to=Category, on_delete=models.DO_NOTHING)  # Field name made lowercase.
+    # All field names made lowercase.
+    user_permission_id = models.AutoField(db_column='User_Permission_ID', primary_key=True)
+    # user_id = models.IntegerField(db_column='User_ID', blank=True, null=True)
+    # category_id = models.IntegerField(db_column='Category_ID', blank=True, null=True)
+    user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(to=Category, on_delete=models.DO_NOTHING)
 
     class Meta:
         managed = False
@@ -127,20 +127,20 @@ class UserPermissions(models.Model):
 
 
 class IndicatorList(models.Model):
-    indicator_id = models.AutoField(db_column='Indicator_ID', primary_key=True)  # Field name made lowercase.
-    old_indicator_title = models.CharField(db_column='Old_Indicator_Title', max_length=255)  # Field name made lowercase.
-    indicator_title = models.CharField(db_column='Indicator_Title', max_length=255)  # Field name made lowercase.
-    # category_id = models.IntegerField(db_column='Category_ID')  # Field name made lowercase.
-    # unit_id = models.IntegerField(db_column='Unit_ID')  # Field name made lowercase.
-    # data_type_id = models.IntegerField(db_column='Data_Type_ID')  # Field name made lowercase.
-    # val_multiplier_id = models.IntegerField(db_column='Val_Multiplier_ID')  # Field name made lowercase.
-    active = models.BooleanField(db_column='Active', blank=True, null=True)  # Field name made lowercase.
-    # summary_type_id = models.IntegerField(db_column='Summary_Type_ID')  # Field name made lowercase.
-    category = models.ForeignKey(to=Category, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    unit = models.ForeignKey(to=Unit, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    data_type = models.ForeignKey(to=DataType, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    val_multiplier = models.ForeignKey(to=ValMultiplier, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    summary_type = models.ForeignKey(to=DataSummaryType, on_delete=models.DO_NOTHING)  # Field name made lowercase.
+    indicator_id = models.AutoField(db_column='Indicator_ID', primary_key=True)
+    old_indicator_title = models.CharField(db_column='Old_Indicator_Title', max_length=255)
+    indicator_title = models.CharField(db_column='Indicator_Title', max_length=255)
+    # category_id = models.IntegerField(db_column='Category_ID')
+    # unit_id = models.IntegerField(db_column='Unit_ID')
+    # data_type_id = models.IntegerField(db_column='Data_Type_ID')
+    # val_multiplier_id = models.IntegerField(db_column='Val_Multiplier_ID')
+    active = models.BooleanField(db_column='Active', blank=True, null=True)
+    # summary_type_id = models.IntegerField(db_column='Summary_Type_ID')
+    category = models.ForeignKey(to=Category, on_delete=models.DO_NOTHING)
+    unit = models.ForeignKey(to=Unit, on_delete=models.DO_NOTHING)
+    data_type = models.ForeignKey(to=DataType, on_delete=models.DO_NOTHING)
+    val_multiplier = models.ForeignKey(to=ValMultiplier, on_delete=models.DO_NOTHING)
+    summary_type = models.ForeignKey(to=DataSummaryType, on_delete=models.DO_NOTHING)
 
     class Meta:
         managed = False
@@ -152,16 +152,16 @@ class IndicatorList(models.Model):
 
 
 class IndicatorData(models.Model):
-    record_id = models.AutoField(db_column='Record_ID', primary_key=True)  # Field name made lowercase.
-    # indicator_id = models.IntegerField(db_column='Indicator_ID')  # Field name made lowercase.
-    # year_month_id = models.IntegerField(db_column='Year_Month_ID')  # Field name made lowercase.
-    val = models.FloatField(db_column='Val', blank=True, null=True)  # Field name made lowercase.
-    created_date = models.DateTimeField(db_column='Created_Date')  # Field name made lowercase.
-    updated_date = models.DateTimeField(db_column='Updated_Date')  # Field name made lowercase.
-    # update_user_id = models.IntegerField(db_column='Update_User_ID')  # Field name made lowercase.
-    indicator = models.ForeignKey(to=IndicatorList, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    year_month = models.ForeignKey(to=YearMonth, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    update_user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)  # Field name made lowercase.
+    record_id = models.AutoField(db_column='Record_ID', primary_key=True)
+    # indicator_id = models.IntegerField(db_column='Indicator_ID')
+    # year_month_id = models.IntegerField(db_column='Year_Month_ID')
+    val = models.FloatField(db_column='Val', blank=True, null=True)
+    created_date = models.DateTimeField(db_column='Created_Date')
+    updated_date = models.DateTimeField(db_column='Updated_Date')
+    # update_user_id = models.IntegerField(db_column='Update_User_ID')
+    indicator = models.ForeignKey(to=IndicatorList, on_delete=models.DO_NOTHING)
+    year_month = models.ForeignKey(to=YearMonth, on_delete=models.DO_NOTHING)
+    update_user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)
 
     class Meta:
         managed = False
@@ -181,9 +181,9 @@ INSERT INTO dbo.Admins
 ([User_ID], [Active]) VALUEs (67, 1)
 """
 class Admins(models.Model):
-    admin_id = models.AutoField(db_column='Admin_ID', primary_key=True)  # Field name made lowercase.
-    user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)  # Field name made lowercase.
-    active = models.BooleanField(db_column='Active', blank=False, null=False)  # Field name made lowercase.
+    admin_id = models.AutoField(db_column='Admin_ID', primary_key=True)
+    user = models.ForeignKey(to=Users, on_delete=models.DO_NOTHING)
+    active = models.BooleanField(db_column='Active', blank=False, null=False)
 
     class Meta:
         managed = False
