3b4a422a7ef37e34cb8ba99348e93024f9a2db95
yizongk <yizongk@gmail.com>
2020-10-21 16:32:20 -0400

    Refactor sendCellToServer() to pass props to callbacks of sentJsonBlobToApi()

--- PMU_DjangoWebApps/static/PMU_DjangoWebApps/js/CellEditSave.js
+++ PMU_DjangoWebApps/static/PMU_DjangoWebApps/js/CellEditSave.js
@@ -181,10 +181,20 @@ function sentJsonBlobToApi( json_blob, api_url, successCallbackFct=function() { 
     });
 }
 
+function finishEditMode(td_node, cell_html_type) {
+    if (cell_html_type == "input") {
+        finishCellEditMode(td_node);
+    } else if (cell_html_type == "select") {
+        finishCellSelectMode(td_node)
+    } else {
+        console.log(`Warning: Unknown finish cell mode: ${cell_html_type}`)
+    }
+}
+
 // THIS IS THE ENTRY POINT
 // successCallbackFct must take in for its first param, the td_node, and then for its second param, the json_response obj. successCallbackFct can be used to do any work after a successful api call, such as updating some element on the html beside the new cell value (The new cell value is updated to the edited cell during the sendCellToServer() function).
 // #@TODO refactor this to use the sentJsonBlobToApi() like sendModalFormDataToServer() and deleteRecordToServer()
-function sendCellToServer( node, api_url, cell_html_type, successCallbackFct=function() { return; } ) {
+function sendCellToServer( node, api_url, cell_html_type ) {
     // console.log(id, new_value, table, column, td_node, old_val);
     var old_val = $(node).attr("old_value")
     var new_value = $(node).val();
@@ -195,66 +205,48 @@ function sendCellToServer( node, api_url, cell_html_type, successCallbackFct=fun
     table = td_node.data("table")
     column = td_node.data("column")
 
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
-            }
-        }
-    });
+    json_obj_to_server = {
+        "id": id,
+        "new_value": new_value,
+        "table": table,
+        "column": column
+    }
 
-    $.ajax({
-        url: api_url,
-        type: "POST",
-        data: {
-            id: id,
-            new_value: new_value,
-            table: table,
-            column: column,
-        },
-    })
-    .done(function (json_response) {
-        // console.log("JSON RESPONSE", json_response);
-        if (json_response["post_success"] == false) {
-            td_node.html(old_val);
-            console.log(`Error: Ajax calling '${api_url}'\nServer Response: ${json_response["post_msg"]}`);
-            alert(`Something went wrong while trying to send cell data to server.\nPlease contact ykuang@dot.nyc.gov if this error continues:\n\nAjax calling '${api_url}'\nServer Response:\n\n${json_response["post_msg"]}`);
+    props = {
+        'td_node': td_node,
+        'old_val': old_val,
+        'new_value': new_value,
+        'cell_html_type': cell_html_type,
+    }
 
-            // Set status light and error message to red and response error msg
-            setDatabaseStatus(good=false, msg=json_response["post_msg"]);
-        } else { // Api call successful
-            td_node.html(new_value);
-            successCallbackFct(td_node, json_response);
-            setDatabaseStatus(good=true, msg="");
-        }
-        // #@TODO maybe this can be left outside of the .done and .fail scope, since in either situation, you do the same work
-        if (cell_html_type == "input") {
-            finishCellEditMode(td_node);
-        } else if (cell_html_type == "select") {
-            finishCellSelectMode(td_node)
-        } else {
-            console.log(`Warning: Unknown finish cell mode: ${cell_html_type}`)
-        }
+    sentJsonBlobToApi( json_obj_to_server, api_url, function(json_response, props) {
+        // successful api call call-back fct
+        td_node = props['td_node']
+        new_value = props['new_value']
+        cell_html_type = props['cell_html_type']
 
-        return true;
-    })
-    .fail(function (jqXHR) {
+        td_node.html(new_value);
+        finishEditMode(td_node, cell_html_type)
+    }, function(json_response, props) {
+        // bad api call call-back fct
+        td_node = props['td_node']
+        old_val = props['old_val']
+        cell_html_type = props['cell_html_type']
+
         td_node.html(old_val);
-        var errorMessage = `Server might be down, try to reload the web page to confirm. If error is still happening, contact ykuang@dot.nyc.gov\n xhr response: ${jqXHR.status}\n xhr response text: ${jqXHR.responseText}`;
-        setDatabaseStatus(good=false, msg=errorMessage);
-        if (cell_html_type == "input") {
-            finishCellEditMode(td_node);
-        } else if (cell_html_type == "select") {
-            finishCellSelectMode(td_node)
-        } else {
-            console.log(`Warning: Unknown finish cell mode: ${cell_html_type}`)
-        }
+        finishEditMode(td_node, cell_html_type)
+    },
+    function(jqXHR, props) {
+        // bad ajax call
+        td_node = props['td_node']
+        old_val = props['old_val']
+        cell_html_type = props['cell_html_type']
 
-        console.log(`Ajax Post: Error Occured: ${errorMessage}`);
-        alert(`Ajax Post: Error Occured:\n\n${errorMessage}`);
-        return false;
-    });
+        td_node.html(old_val);
+        finishEditMode(td_node, cell_html_type)
+    },
+    props );
+
 };
 
 function sendModalFormDataToServer( json_blob, api_url, successCallbackFct=function() { return; }, failCallbackFct=function() { return; } ) {
