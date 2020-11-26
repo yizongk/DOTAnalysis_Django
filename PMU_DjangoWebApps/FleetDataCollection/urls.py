e046939b9cdc66aba436c6a9e407c97cfa818d85
yizongk <yizongk@gmail.com>
2020-11-26 03:26:25 -0500

    Rename api GetM5DataList to GetM5LookUpDataList and api GetEmpDataList to GetEmpLookUpDataList

--- PMU_DjangoWebApps/FleetDataCollection/urls.py
+++ PMU_DjangoWebApps/FleetDataCollection/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView, ContactPageView, DriverAndTypeAssignmentConfirmationPageView, GetPermittedEmpDataList, UpdateM5DriverVehicleDataConfirmations, GetM5DataList, GetEmpDataList
+from .views import HomePageView, AboutPageView, ContactPageView, DriverAndTypeAssignmentConfirmationPageView, GetPermittedEmpDataList, UpdateM5DriverVehicleDataConfirmations, GetM5LookUpDataList, GetEmpLookUpDataList
 urlpatterns = [
 
     path('', HomePageView.as_view(), name='fleetdatacollection_home_view'),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('driver_and_type_confirmation', DriverAndTypeAssignmentConfirmationPageView.as_view(), name='fleetdatacollection_driver_and_type_confirmation_view'),
     path('get_permitted_pms_list', views.GetPermittedEmpDataList, name='fleetdatacollection_get_permitted_pms_list'),
     path('update_m5_driver_vehicle_data_confirmations', views.UpdateM5DriverVehicleDataConfirmations, name='fleetdatacollection_update_m5_driver_vehicle_data_confirmations'),
-    path('get_m5_list', views.GetM5DataList, name='fleetdatacollection_get_m5_list'),
-    path('get_pms_list', views.GetEmpDataList, name='fleetdatacollection_get_pms_list'),
+    path('get_m5_list', views.GetM5LookUpDataList, name='fleetdatacollection_get_m5_list'),
+    path('get_pms_list', views.GetEmpLookUpDataList, name='fleetdatacollection_get_pms_list'),
 
 ]
