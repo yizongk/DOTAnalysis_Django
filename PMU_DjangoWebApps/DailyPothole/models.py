5f72c1820b09f058a481a515809a18a8af583407
yizongk <yizongk@gmail.com>
2021-06-18 14:29:28 -0400

    DailyPothole: Add database config and models.py

--- PMU_DjangoWebApps/DailyPothole/models.py
+++ PMU_DjangoWebApps/DailyPothole/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+# All field names made lowercase.
+class TblBoros(models.Model):
+    boros_id = models.AutoField(db_column='BorosId', primary_key=True)
+    boro_order = models.IntegerField(db_column='BoroOrder', unique=True)
+    boro_code = models.CharField(db_column='BoroCode', max_length=1, blank=False, null=False, unique=True)
+    boro_long = models.CharField(db_column='BoroLong', max_length=20, blank=False, null=False)
+
+    class Meta:
+        managed = False
+        db_table = 'tblBoros'
+
+    def __str__(self):
+        return self.boro_code
+
+
+class TblOperation(models.Model):
+    operation_id = models.AutoField(db_column='OperationId', primary_key=True)
+    operation_code = models.IntegerField(db_column='OperationCode', blank=False, null=False, unique=True)
+    operation = models.CharField(db_column='Operation', max_length=50, blank=False, null=False)
+
+    class Meta:
+        managed = False
+        db_table = 'tblOperation'
+
+    def __str__(self):
+        return self.operation
+
+
+class TblUserList(models.Model):
+    user_id = models.AutoField(db_column='UserId', primary_key=True)
+    username = models.CharField(db_column='Username', max_length=50, unique=True)
+    operation_id = models.ForeignKey(to=TblOperation, to_field='operation_id', db_column='OperationId', on_delete=models.DO_NOTHING)
+    boros_id = models.ForeignKey(to=TblBoros, to_field='boros_id', db_column='BorosId', on_delete=models.DO_NOTHING)
+    is_admin = models.BooleanField(db_column='IsAdmin')
+
+    class Meta:
+        managed = False
+        db_table = 'tblUserList'
+
+    def __str__(self):
+        return self.username
+
+
+class TblPotholeMaster(models.Model):
+    pothole_master_id = models.AutoField(db_column='PotholeMasterId', primary_key=True)
+    repair_date = models.DateField(db_column='RepairDate')
+    operation_id = models.ForeignKey(to=TblOperation, to_field='operation_id', db_column='OperationId', on_delete=models.DO_NOTHING)
+    boros_id = models.ForeignKey(to=TblBoros, to_field='boros_id', db_column='BorosId', on_delete=models.DO_NOTHING)
+    repair_crew_count = models.IntegerField(db_column='RepairCrewCount')
+    holes_repaired = models.IntegerField(db_column='HolesRepaired')
+    daily_crew_count = models.IntegerField(db_column='DailyCrewCount')
+    last_modified_stamp = models.DateTimeField(db_column='LastModifiedStamp')
+    last_modified_by_user_id = models.ForeignKey(to=TblUserList, to_field='user_id', db_column='LastModifiedByUserId', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tblPotholeMaster'
+
+    def __str__(self):
+        return self.last_edited_by_user_id
--- PMU_DjangoWebApps/PMU_DjangoWebApps/settings.py
+++ PMU_DjangoWebApps/PMU_DjangoWebApps/settings.py
@@ -231,6 +231,34 @@ else:
         },
     }
 
+if DailyPothole_UseWinAuth:
+    DATABASES['DailyPothole'] = {
+        'ENGINE':       'sql_server.pyodbc',
+        'HOST' :        DailyPothole_SQLServerHost,
+        'NAME' :        DailyPothole_SQLServerDbName,
+        'AUTOCOMMIT' :  True,               # Set this to False if you want to disable Django's transaction management and implement your own.
+        'ATOMIC_REQUESTS' : True,           # All views/request are not wrapped in a transcation on the database, if response is produced without fails, will commit the transaction, else rolls back the transaction, ref: https://docs.djangoproject.com/en/3.0/topics/db/transactions/
+
+        'OPTIONS' : {
+            'driver' :      'SQL Server Native Client 11.0',
+        },
+    }
+else:
+    DATABASES['DailyPothole'] = {
+        'ENGINE':       'sql_server.pyodbc',
+        'HOST' :        DailyPothole_SQLServerHost,
+        'NAME' :        DailyPothole_SQLServerDbName,
+        'USER' :        DailyPothole_SQLServerUID,
+        'PASSWORD' :    DailyPothole_SQLServerPWD,
+        'AUTOCOMMIT' :  True,               # Set this to False if you want to disable Django's transaction management and implement your own.
+        'ATOMIC_REQUESTS' : True,           # All views/request are not wrapped in a transcation on the database, if response is produced without fails, will commit the transaction, else rolls back the transaction, ref: https://docs.djangoproject.com/en/3.0/topics/db/transactions/
+
+        'OPTIONS' : {
+            'driver' :      'SQL Server Native Client 11.0',
+        },
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
