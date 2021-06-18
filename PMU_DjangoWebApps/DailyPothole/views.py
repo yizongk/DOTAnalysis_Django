b0d5cc8dab06fd20b9a81b169e4189d2ce650d7e
yizongk <yizongk@gmail.com>
2021-06-18 14:36:55 -0400

    DailyPothole: Add main data table view

--- PMU_DjangoWebApps/DailyPothole/views.py
+++ PMU_DjangoWebApps/DailyPothole/views.py
@@ -2,6 +2,59 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import generic
 
+from .models import *
+from django.http import JsonResponse
+
+
+## Return a list of Operations that the client has access to. Returns not limited to 1 Operation, can be multiple.
+def get_user_operation_permission(username):
+    try:
+        operation_permission_query = TblUserList.objects.using('DailyPothole').filter(
+            username__exact=username
+        ).order_by('operation_id')
+
+        if operation_permission_query.count() > 0:
+            return {
+                "success": True,
+                "err": "",
+                "operation_permission_list": [each.operation_id for each in operation_permission_query],
+            }
+        return {
+            "success": False,
+            "err": "Cannot find any allowed unit_number for '{}'".format(username),
+        }
+    except Exception as e:
+        print("Exception: DailyPothole: get_user_operation_permission(): {}".format(e))
+        return {
+            "success": False,
+            "err": 'Exception: DailyPothole: get_user_operation_permission(): {}'.format(e),
+        }
+
+
+## Check if remote user is admin and is active
+def user_is_active_admin(username):
+    try:
+        admin_query = TblUserList.objects.using('DailyPothole').filter(
+            username=username,
+            is_admin=True, ## Filters for Admins
+        )
+        if admin_query.count() > 0:
+            return {
+                "isAdmin": True,
+                "err": "",
+            }
+        return {
+            "isAdmin": False,
+            "err": '{} is not an active Admin'.format(username),
+        }
+    except Exception as e:
+        print("Exception: user_is_active_admin(): {}".format(e))
+        return {
+            "isAdmin": None,
+            "err": 'Exception: user_is_active_admin(): {}'.format(e),
+        }
+
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'DailyPothole.template.home.html'
@@ -18,15 +71,18 @@ class HomePageView(TemplateView):
             # context["client_is_admin"] = False
             return context
 
+
 class AboutPageView(TemplateView):
     template_name = 'DailyPothole.template.about.html'
+
 
 class ContactPageView(TemplateView):
     template_name = 'DailyPothole.template.contact.html'
 
+
 class DataCollectionPageView(generic.ListView):
     template_name = 'DailyPothole.template.datacollection.html'
-    context_object_name = 'pothole_data'
+    context_object_name = 'daily_pothole'
 
     req_success = False
     err_msg = ""
@@ -34,35 +90,28 @@ class DataCollectionPageView(generic.ListView):
     client_is_admin = False
 
     def get_queryset(self):
-        ## Check for Active Admins
-        # is_active_admin = user_is_active_admin(self.request.user)
-        # if is_active_admin["success"] == True:
-        #     self.client_is_admin = True
-        # else:
-        #     self.req_success = False
+        # Check for Active Admins
+        self.client_is_admin = user_is_active_admin(self.request.user)["isAdmin"]
 
         ## Get the core data
         try:
             if self.client_is_admin:
-                # pothole_data = M5DriverVehicleDataConfirmations.objects.using('FleetDataCollection').all().order_by('unit_number')
-                pothole_data = None
+                pothole_data = TblPotholeMaster.objects.using('DailyPothole').all().order_by('repair_date', 'boros_id', 'operation_id')
             else:
-                # allowed_unit_number_list_obj = get_allowed_list_of_unit_numbers(self.request.user)
-                # if allowed_unit_number_list_obj['success'] == False:
-                #     raise ValueError('get_allowed_list_of_unit_numbers() failed: {}'.format(allowed_unit_number_list_obj['err']))
-                # else:
-                #     allowed_unit_number_list = allowed_unit_number_list_obj['unit_number_list']
+                operation_for_user_list = get_user_operation_permission(self.request.user)
+                if operation_for_user_list['success'] == False:
+                    raise ValueError('get_user_operation_permission() failed: {}'.format(operation_for_user_list['err']))
+                else:
+                    allowed_operation_list = operation_for_user_list['operation_permission_list']
 
-                # pothole_data = M5DriverVehicleDataConfirmations.objects.using('FleetDataCollection').filter(
-                #     unit_number__in=allowed_unit_number_list,
-                # ).order_by('unit_number')
-                pothole_data = None
+                pothole_data = TblPotholeMaster.objects.using('DailyPothole').filter(
+                    operation_id__in=allowed_operation_list,
+                ).order_by('repair_date', 'boros_id', 'operation_id')
         except Exception as e:
             self.req_success = False
             self.err_msg = "Exception: DateCollectionPageView(): get_queryset(): {}".format(e)
             print(self.err_msg)
-            # return M5DriverVehicleDataConfirmations.objects.none()
-            return None
+            return TblPotholeMaster.objects.none()
 
         self.req_success = True
         return pothole_data
