2b3296891ac82a2209318305c9b5b2b57a867086
yizongk <yizongk@gmail.com>
2022-05-12 18:14:25 -0400

    UserManagement: Add ManageWebAppsPageView to views.py and setup its html

--- WebApps/UserManagement/urls.py
+++ WebApps/UserManagement/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import HomePageView, AboutPageView, ContactPageView, ManageWebAppsPageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='UserManagement_home_view'),
     path('about', AboutPageView.as_view(), name='UserManagement_about_view'),
     path('contact', ContactPageView.as_view(), name='UserManagement_contact_view'),
+    path('manage_web_apps', ManageWebAppsPageView.as_view(), name='UserManagement_manage_web_apps_view'),
 ]
--- WebApps/UserManagement/views.py
+++ WebApps/UserManagement/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
 from .models import *
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
 
@@ -65,4 +67,53 @@ class ContactPageView(TemplateView):
         context["get_success"]      = self.get_success
         context["get_error"]        = self.get_error
         context["client_is_admin"]  = self.client_is_admin
+        return context
+
+
+class ManageWebAppsPageView(generic.ListView):
+    template_name           = 'UserManagement.template.managewebapps.html'
+
+    get_success             = False
+    get_error               = None
+    client_is_admin         = False
+
+    ag_grid_col_def_json    = None
+    rows_data_json          = None
+
+    def get_queryset(self):
+        ## Check for Active Admins
+        self.client_is_admin = user_is_active_admin(self.request.user)
+
+        ## Get the core data
+        try:
+            ag_grid_col_def = [ ## Need to format this way for AG Grid
+                {'headerName': 'Web App ID'     , 'field': 'web_app_id'     , 'suppressMovable': True , 'lockPinned': True , 'cellClass': 'left-pinned' , 'pinned': 'left' , 'width': 80}
+                ,{'headerName': 'Web App Name'  , 'field': 'web_app_name'   , 'suppressMovable': True , 'lockPinned': True}
+                ,{'headerName': 'Is Active'     , 'field': 'is_active'      , 'suppressMovable': True , 'lockPinned': True}
+            ]
+
+            fields_list = [each['field'] for each in ag_grid_col_def]
+
+            web_apps = WebApps.objects.using('default').all().order_by('web_app_id').values(*fields_list)
+
+            self.ag_grid_col_def_json   = json.dumps(list(ag_grid_col_def)  , cls=DjangoJSONEncoder)
+            self.rows_data_json         = json.dumps(list(web_apps)         , cls=DjangoJSONEncoder)
+
+        except Exception as e:
+            self.get_success    = False
+            self.get_error      = f"ManageWebAppsPageView(): get_queryset(): {e}"
+            return None
+
+        self.get_success = True
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["get_success"]          = self.get_success
+        context["get_error"]            = self.get_error
+        context["client_is_admin"]      = self.client_is_admin
+
+        context["ag_grid_col_def_json"] = self.ag_grid_col_def_json
+        context["rows_data_json"]       = self.rows_data_json
         return context
