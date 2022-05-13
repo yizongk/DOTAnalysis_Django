a028cc4c56bab9f0d1647e433847eb1c97bf0ae2
yizongk <yizongk@gmail.com>
2022-05-13 16:41:09 -0400

    UserManagement: Add TestViewPagesResponse to tests.py and fix the associated views in views.py

--- WebApps/UserManagement/tests.py
+++ WebApps/UserManagement/tests.py
@@ -1,9 +1,28 @@
 from .models import *
-from WebAppsMain.settings import TEST_WINDOWS_USERNAME
-from WebAppsMain.testing_utils import HttpPostTestCase, HttpGetTestCase
+from WebAppsMain.settings import TEST_WINDOWS_USERNAME, WEB_APP_NAME_USER_MANAGEMENT
+from WebAppsMain.testing_utils import HttpPostTestCase, HttpGetTestCase, grant_membership_admin_status, remove_membership_admin_status, grant_membership_active_status, remove_membership_active_status
 from django.core.exceptions import ObjectDoesNotExist
+import json
 
 # Create your tests here.
+
+
+def set_up():
+    """Give admin status to @windows_username and make membership active. Defaults to TEST_WINDOWS_USERNAME and WEB_APP_NAME_USER_MANAGEMENT"""
+    try:
+        grant_membership_admin_status(windows_username=TEST_WINDOWS_USERNAME, web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        grant_membership_active_status(windows_username=TEST_WINDOWS_USERNAME, web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+    except Exception as e:
+        raise ValueError(f"set_up(): {e}")
+
+
+def tear_down():
+    """Removes admin status from @windows_username and make membership inactive. Defaults to TEST_WINDOWS_USERNAME and WEB_APP_NAME_USER_MANAGEMENT"""
+    try:
+        remove_membership_admin_status(windows_username=TEST_WINDOWS_USERNAME, web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        remove_membership_active_status(windows_username=TEST_WINDOWS_USERNAME, web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+    except Exception as e:
+        raise ValueError(f"tear_down(): {e}")
 
 
 class TestModelActiveDirectory(HttpGetTestCase):
@@ -94,3 +113,100 @@ class TestModelActiveDirectory(HttpGetTestCase):
         else:
             self.assertTrue(False
                 ,f"TestModelActiveDirectory: Model cannot allow update()")
+
+
+class TestViewPagesResponse(HttpGetTestCase):
+    @classmethod
+    def setUpClass(self):
+        set_up()
+
+        self.regular_views = [
+            'usermanagement_home_view',
+            'usermanagement_about_view',
+            'usermanagement_contact_view',
+        ]
+
+        self.admin_views = [
+            'usermanagement_manage_web_apps_view',
+        ]
+
+        self.additional_context_requirements = [
+            ## The below are admin views
+            {
+                'view'                      : 'usermanagement_manage_web_apps_view'
+                ,'additional_context_keys'  : [
+                                                'ag_grid_col_def_json'
+                                                ,'rows_data_json'
+                                            ]
+                ,'qa_fct'                   : self.__assert_additional_context_qa_manage_web_apps
+            }
+        ]
+
+    @classmethod
+    def tearDownClass(self):
+        tear_down()
+
+    def test_views_response_status_200(self):
+        """Test normal user"""
+        remove_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_response_status_200()
+
+        """Test admin user"""
+        grant_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_response_status_200()
+
+    def test_views_response_user_admin_restriction(self):
+        """Test inactive user webapp membership (Non-Admin), should have NO access to regular or admin views"""
+        remove_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        remove_membership_active_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_inactive_user_no_access_on_normal_and_admin_view()
+
+        """Test inactive user webapp membership (Admin), should have NO access to regular or admin views"""
+        grant_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        remove_membership_active_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_inactive_user_no_access_on_normal_and_admin_view()
+
+        """Test active user webapp membership (Non-Admin), should only have access to regular views"""
+        grant_membership_active_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        remove_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_user_access_on_normal_and_admin_view()
+
+        """Test active user webapp membership (Admin), should have access to regular and admin views"""
+        grant_membership_active_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        grant_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_admin_access_on_normal_and_admin_view()
+
+    def test_views_response_data(self):
+        """
+            Test views to have the required GET request context data
+            Some views have additional context data, need to test for those here
+        """
+        # Test normal user
+        remove_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_additional_context_data(additional_requirements=self.additional_context_requirements)
+
+        # Test admin user
+        grant_membership_admin_status(web_app_name=WEB_APP_NAME_USER_MANAGEMENT)
+        self.assert_additional_context_data(additional_requirements=self.additional_context_requirements)
+
+    def __assert_additional_context_qa_manage_web_apps(self, response):
+        ## Make sure the ag_grid_col_def_json got all the required fields
+        ag_grid_col_def_dict    = json.loads(response.context_data['ag_grid_col_def_json'])
+        from_api_fields         = set(each['field'] for each in ag_grid_col_def_dict)
+        required_fields         = set([
+            'web_app_id'
+            ,'web_app_name'
+            ,'is_active'
+        ])
+        if len(from_api_fields) > len(required_fields):
+            raise ValueError(f"usermanagement_manage_web_apps_view: context variable ag_grid_col_def_json got back more fields than expected. These are the unexpected fields: {from_api_fields - required_fields}")
+        self.assertTrue(from_api_fields == required_fields
+            ,f'usermanagement_manage_web_apps_view: context variable ag_grid_col_def_json is missing some fields: {required_fields -  from_api_fields}')
+
+        ## Make sure rows_data_json has at least the app name of the UserManagement web app that we are testing
+        row_data = json.loads(response.context_data['rows_data_json'])
+        self.assertTrue(len(row_data) > 0
+            ,f"usermanagement_manage_web_apps_view: context variable rows_data_json doesn't have any data")
+        self.assertTrue(sorted(required_fields) == sorted(row_data[0].keys())
+            ,f'usermanagement_manage_web_apps_view: context variable rows_data_json is missing some fields: {required_fields -  set(row_data[0].keys())}')
+
--- WebApps/UserManagement/views.py
+++ WebApps/UserManagement/views.py
@@ -5,25 +5,56 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 import json
 from django.core.serializers.json import DjangoJSONEncoder
+from WebAppsMain.settings import WEB_APP_NAME_USER_MANAGEMENT
 
 # Create your views here.
 
 
-APP_NAME = "UserManagement"
+def user_is_enabled(username=None):
+    """check active directory to see if user is enabled"""
+    try:
+        ad_user = ActiveDirectory.objects.using('default').get(
+            windows_username__exact=username
+        )
+
+        return ad_user.enabled
+    except Exception as e:
+        raise ValueError(f"user_is_enabled(): {e}")
 
 
 def user_is_active_admin(username=None):
+    """check user app membership to see if membership is active and is admin"""
     try:
-        user = WebAppUserMemberships.objects.using('default').get(
+        if not user_is_enabled(username=username):
+            return False
+
+        user_app_membership = WebAppUserMemberships.objects.using('default').get(
             windows_username__windows_username__exact=username
-            ,web_app_id__web_app_name__exact=APP_NAME
+            ,web_app_id__web_app_name__exact=WEB_APP_NAME_USER_MANAGEMENT
         )
-        if user.is_active:
-            return user.is_admin
+
+        if user_app_membership.is_active:
+            return user_app_membership.is_admin
         else:
             return False
     except Exception as e:
         raise ValueError(f"user_is_active_admin(): {e}")
+
+
+def user_is_active(username=None):
+    """check user app membership to see if membership is active"""
+    try:
+        if not user_is_enabled(username=username):
+            return False
+
+        user_app_membership = WebAppUserMemberships.objects.using('default').get(
+            windows_username__windows_username__exact=username
+            ,web_app_id__web_app_name__exact=WEB_APP_NAME_USER_MANAGEMENT
+        )
+
+        return user_app_membership.is_active
+    except Exception as e:
+        raise ValueError(f"user_is_active(): {e}")
 
 
 class HomePageView(TemplateView):
@@ -33,9 +64,13 @@ class HomePageView(TemplateView):
     client_is_admin = None
 
     def get_context_data(self, **kwargs):
+        if not user_is_active(username=self.request.user):
+            self.get_success = False
+            self.get_error = f"'{self.request.user}' is not an active user of the application"
+
         ## Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        self.client_is_admin = user_is_active_admin(self.request.user)
+        self.client_is_admin = user_is_active_admin(username=self.request.user)
         context["client_is_admin"]  = self.client_is_admin
         context["get_success"]      = self.get_success
         context["get_error"]        = self.get_error
@@ -49,6 +84,10 @@ class AboutPageView(TemplateView):
     client_is_admin = None
 
     def get_context_data(self, **kwargs):
+        if not user_is_active(username=self.request.user):
+            self.get_success = False
+            self.get_error = f"'{self.request.user}' is not an active user of the application"
+
         context = super().get_context_data(**kwargs)
         context["get_success"]      = self.get_success
         context["get_error"]        = self.get_error
@@ -63,6 +102,10 @@ class ContactPageView(TemplateView):
     client_is_admin = None
 
     def get_context_data(self, **kwargs):
+        if not user_is_active(username=self.request.user):
+            self.get_success = False
+            self.get_error = f"'{self.request.user}' is not an active user of the application"
+
         context = super().get_context_data(**kwargs)
         context["get_success"]      = self.get_success
         context["get_error"]        = self.get_error
@@ -81,11 +124,17 @@ class ManageWebAppsPageView(generic.ListView):
     rows_data_json          = None
 
     def get_queryset(self):
-        ## Check for Active Admins
-        self.client_is_admin = user_is_active_admin(self.request.user)
-
-        ## Get the core data
         try:
+            ## Check for active user
+            if not user_is_active(username=self.request.user):
+                raise ValueError(f"'{self.request.user}' is not an active user of the application")
+
+            ## Check for Active Admins
+            self.client_is_admin = user_is_active_admin(username=self.request.user)
+            if not self.client_is_admin:
+                raise ValueError(f"'{self.request.user}' is not an admin")
+
+            ## Get the core data
             ag_grid_col_def = [ ## Need to format this way for AG Grid
                 {'headerName': 'Web App ID'     , 'field': 'web_app_id'     , 'suppressMovable': True , 'lockPinned': True , 'cellClass': 'left-pinned' , 'pinned': 'left' , 'width': 80}
                 ,{'headerName': 'Web App Name'  , 'field': 'web_app_name'   , 'suppressMovable': True , 'lockPinned': True}
