9712e4ee6082e62cb61d85fd68c6e0fb5920078d
yizongk <yizongk@gmail.com>
2022-05-13 13:08:44 -0400

    UserManagement: Refactor url names

--- WebApps/UserManagement/urls.py
+++ WebApps/UserManagement/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 from .views import HomePageView, AboutPageView, ContactPageView, ManageWebAppsPageView
 urlpatterns = [
-    path('', HomePageView.as_view(), name='UserManagement_home_view'),
-    path('about', AboutPageView.as_view(), name='UserManagement_about_view'),
-    path('contact', ContactPageView.as_view(), name='UserManagement_contact_view'),
-    path('manage_web_apps', ManageWebAppsPageView.as_view(), name='UserManagement_manage_web_apps_view'),
+    path('', HomePageView.as_view(), name='usermanagement_home_view'),
+    path('about', AboutPageView.as_view(), name='usermanagement_about_view'),
+    path('contact', ContactPageView.as_view(), name='usermanagement_contact_view'),
+    path('manage_web_apps', ManageWebAppsPageView.as_view(), name='usermanagement_manage_web_apps_view'),
 ]
